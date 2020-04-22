a5d0060259353b734d1052b34931807cf8c2aac1
Simon Lukas <simon.lukas8@web.de>
2020-04-22 15:41:01 +0200

    Größe der Konsole angepasst von 120 auf 80

--- SpaceAttack/Program.vb
+++ SpaceAttack/Program.vb
@@ -5,13 +5,13 @@ Module Program
 
     Sub ErzeugeZeile()
 
-
-        Dim vektor(119) As Char
+        'aktuallisierte Zahlen
+        Dim vektor(79) As Char
         Dim AnzahlBlöcke, Startposition, HindernisBreite, j, h As Integer
         'Zufallszahlen
         Dim Zufallszahl1, Zufallszahl2, Zufallszahl3 As Single
 
-        For j = 0 To 119
+        For j = 0 To 79
             vektor(j) = " "
 
         Next
@@ -19,22 +19,23 @@ Module Program
         'Per Zufall bestimmen, wie viele Hindernisblöcke erscheinen
         Randomize()
         Zufallszahl1 = VBMath.Rnd()
-        'Bilde den Zufallsbereich 0 bis 1 auf 2 bis 15 ab!
-        AnzahlBlöcke = 13 * Zufallszahl1 + 2
-        'Console.WriteLine(x & " " & AnzahlBlöcke)
+        'Bilde den Zufallsbereich 0 bis 1 auf 2 bis 7 ab!
+        AnzahlBlöcke = 5 * Zufallszahl1 + 2
 
         'Position und Breite jedes Hindernisblocks bestimmen!
         For j = 1 To AnzahlBlöcke
+            Randomize()
             Zufallszahl2 = VBMath.Rnd()
-            'Größe des Blocks von 1 bis 8
-            HindernisBreite = 7 * Zufallszahl2 + 1
+            'Größe des Blocks von 1 bis 10
+            HindernisBreite = 9 * Zufallszahl2 + 1
             'Startposition des Blocksfestlegen
+            Randomize()
             Zufallszahl3 = VBMath.Rnd()
             'Position des Blocks bestimmen!
-            Startposition = 119 * Zufallszahl3
+            Startposition = 79 * Zufallszahl3
 
             'Der Vektor darf nicht überlaufen
-            If Startposition + HindernisBreite - 1 <= 119 Then
+            If Startposition + HindernisBreite - 1 <= 79 Then
 
                 'Fülle den Vektor mit "X"
                 For h = 1 To HindernisBreite
@@ -53,9 +54,10 @@ Module Program
     End Sub
 
     Sub Main()
+        'Lege die Größe der Konsole fest!
+        Console.SetWindowSize(80, 24)
 
         Call ErzeugeZeile()
-
 
     End Sub
 End Module
